881da81ca831364461a7036082d00ec7837ee14c
HenriqueDreyer <henriquedreyer@gmail.com>
2024-05-22 19:49:53 -0300

    [FEATURE] Start Spring Project

---
+++ src/main/java/com/dreyer/bradescocodechallenge/BradescoCodeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.dreyer.bradescocodechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BradescoCodeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BradescoCodeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreyer/bradescocodechallenge/BradescoCodeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreyer.bradescocodechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BradescoCodeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
