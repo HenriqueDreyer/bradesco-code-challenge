5fca9ac6ef79bcc6594b437d636a1c7bc825cce9
Henrique Dreyer <henriquedreyer@gmail.com>
2024-05-22 22:15:24 -0300

    [FIX] Ajuste no SecurityConfig

--- src/main/java/com/dreyer/bradescocodechallenge/config/SecurityConfig.java
+++ src/main/java/com/dreyer/bradescocodechallenge/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.dreyer.bradescocodechallenge.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,9 +9,11 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(authorizeConfig  -> {
-            authorizeConfig .requestMatchers("/**").permitAll();
+            authorizeConfig.requestMatchers("/**").permitAll();
         }).build();
     }
 }
