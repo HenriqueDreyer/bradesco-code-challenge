124c2dc59836ab9b8343ffb3006b792f7e9a7bf9
HenriqueDreyer <henriquedreyer@gmail.com>
2024-05-23 19:52:43 -0300

    [FIX] Ajuste no CORS e CSRF

--- src/main/java/com/dreyer/bradescocodechallenge/config/SecurityConfig.java
+++ src/main/java/com/dreyer/bradescocodechallenge/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,8 +13,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(authorizeConfig  -> {
-            authorizeConfig.requestMatchers("/**").permitAll();
-        }).build();
+        return http.authorizeHttpRequests(authorizeConfig -> {
+                    authorizeConfig.requestMatchers("/**").permitAll();
+                })
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
     }
 }
